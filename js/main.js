99b17d6f6916971ce1a0335b36b37cc4db928e54
Steve Dennett <sdennett@wayfair.com>
2016-12-28 15:35:15 -0500

    switchPage function is leaner

--- js/main.js
+++ js/main.js
@@ -7,16 +7,32 @@ function toArray(str) {
 var movies = toArray('The Titanic, Forrest Gump, Saving Private Ryan');
 var nineties = toArray('Stretch Armstrong, Nickelodeon, Salute Your Shorts');
 
-console.log(movies);
 
-function nextPage() {
+/////////////////
+/// Begin Game
+/////////////////
+
+var currentPageNum = 0;
+
+function switchPage() {
   var currentPage = document.querySelector('.js-slider');
-  var currentPageNum = parseInt(currentPage.dataset.currentpage);
-  currentPage.classList.remove(`HeadsUp-page--slide${currentPageNum}00`);
+  var pagination = document.querySelector('.js-pagination');
 
-  currentPageNum += 1;
-  currentPage.dataset.currentpage = currentPageNum;
+  var direction = this.dataset !== undefined && this.dataset.direction === 'prev' ? -1 : 1;
+  currentPageNum += direction;
+
+  currentPage.classList.remove(`HeadsUp-page--slide${currentPageNum - direction}00`);
   currentPage.classList.add(`HeadsUp-page--slide${currentPageNum}00`);
+  pagination.classList.remove(`HeadsUp-pagination--${currentPageNum - direction}`);
+  pagination.classList.add(`HeadsUp-pagination--${currentPageNum}`);
+
+  currentPageNum < 1 ? pagination.classList.remove('is-active') : pagination.classList.add('is-active');
+
+  // disable buttons when page is not active
+  var buttons = document.querySelectorAll('.js-input');
+  buttons.forEach(button => {
+    parseInt(button.parentNode.dataset.page) !== currentPageNum ? button.disabled = true : button.disabled = false;
+  });
 }
 
 function selectGameMode() {
@@ -28,9 +44,11 @@ function quickPlayMode() {
 }
 
 function competitionMode() {
-  console.log('woah this is COMPETITION mode!');
-  nextPage();
+  switchPage();
 }
+
+var paginationPrev = document.querySelector('.js-pagination-prev');
+paginationPrev.addEventListener('click', switchPage);
 
 var inputs = document.querySelectorAll('.js-input');
 inputs.forEach(input => input.addEventListener('click', selectGameMode));
